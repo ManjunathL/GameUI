629cbd20b8b654b4d802bd2083fb75e49e07d656
shruthi <shruthirs30@gmail.com>
2017-05-25 17:18:25 +0530

    -handle thickness message

--- src/main/java/com/mygubbi/game/dashboard/view/proposals/MDW.java
+++ src/main/java/com/mygubbi/game/dashboard/view/proposals/MDW.java
@@ -1395,7 +1395,8 @@ public class MDW extends Window {
             //LOG.info("thickness field value " +thicknessfield.getValue().toString());
             if(Objects.equals(proposalHeader.getBeforeProductionSpecification(), "yes"))
             {
-                if(Objects.equals(module.getHandleThickness(),null) && Objects.equals(module.getHandleThickness(), "Yes"))
+                LOG.info("ssss " +module.getHandleThickness());
+                if(Objects.equals(module.getHandleThickness(),null) && Objects.equals(module.getHandlePresent(), "Yes"))
                 {
                     NotificationUtil.showNotification("Please select thickness before saving", NotificationUtil.STYLE_BAR_ERROR_SMALL);
                     return;
